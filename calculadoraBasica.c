e3df11170cc37abeabbc166b82ecc1827f0a01ce
Ezequiel <ezequielwanzo2508@gmail.com>
2025-12-05 11:30:02 -0300

    Funcion Division Lista

--- calculadoraBasica.c
+++ calculadoraBasica.c
@@ -49,6 +49,8 @@ int main()
                 printf("Error: Division por cero no permitida. Ingrese un segundo numero diferente de cero: ");
                 scanf("%d", &num2);
             }
+            resultado_div = dividir(num1,num2);
+            printf("%d / %d = %f", num1,num2,resultado_div);
             break;
 
         case 5:
@@ -81,6 +83,8 @@ void mostrar_menu()
 }
 
 // Crear las demás funciones aquí
-
+float dividir(int a, int b){
+    return ((float)a)/b;
+}
 
 
